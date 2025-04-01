809d2a6ee5b9977d27edd6f851e23f9f166ea7a4
Luca <hello@lucaelsen.de>
2025-04-01 12:10:25 +0200

    add heading font css

--- web-code/src/App.vue
+++ web-code/src/App.vue
@@ -3,15 +3,18 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+<h1>Discover my <strong>Work</strong></h1>
+
+<h2><strong><i>Nulla esse incididunt</i></strong> sint minim deserunt</h2>
+
+<h3>Inter 16px Do occaecat nulla laborum</h3>
+
+<p>
+  Et Lorem aliquip mollit dolore nostrud proident adipisicing dolor nulla ullamco nulla esse sit magna. Consectetur duis laborum sint in enim. Nostrud est labore consequat ut fugiat aute laboris. Enim eu excepteur culpa deserunt ullamco ad commodo labore proident non aliquip ipsum. Cupidatat enim nisi dolor do pariatur ipsum. Non laboris veniam adipisicing sint sunt nisi fugiat irure sint duis. Id fugiat ex qui nostrud amet est reprehenderit fugiat.
+</p>
+
+
 </template>
 
 <style scoped>
