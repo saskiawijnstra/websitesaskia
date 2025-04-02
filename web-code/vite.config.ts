67350eef1eb9b3155b80a5bf66d98d5d8c10b690
Luca <hello@lucaelsen.de>
2025-04-02 10:56:20 +0200

    add scss support

--- web-code/vite.config.ts
+++ web-code/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
+const currentDir = dirname(fileURLToPath(import.meta.url));
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "${join(currentDir, "./src/scss/_functions.scss")}" as *;`,
+        api: "modern",
+      },
+    },
+  },
   plugins: [vue()],
-})
+});
