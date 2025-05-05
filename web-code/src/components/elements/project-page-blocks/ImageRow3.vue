e08e307e6ddf84032ee8eb63c7e5b0b1035a3266
Luca <hello@lucaelsen.de>
2025-05-05 12:16:59 +0200

    add support for image labels in ImageRow3

--- web-code/src/components/elements/project-page-blocks/ImageRow3.vue
+++ web-code/src/components/elements/project-page-blocks/ImageRow3.vue
@@ -1,25 +1,37 @@
 <template>
   <div class="project-block image-row-3 col-span-12">
     <img
-      v-for="(item, index) in blockData.content"
+      v-for="(item, index) in blockData.content.images"
       :key="index"
       :src="item.image.url"
       :alt="item.image.alt"
       class="image-row-3__image"
     />
+    <label
+      :id="labelId"
+      v-if="blockData.content.label"
+      v-html="blockData.content.label"
+    >
+    </label>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
+
 defineProps({
   blockData: {
     type: Object,
     required: true,
   },
 });
+
+const labelId = computed<string>(() => {
+  return `label-id-${Math.random() * 1000000}`;
+});
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .image-row-3 {
   display: flex;
   gap: 1rem;
@@ -30,5 +42,11 @@ defineProps({
 .image-row-3__image {
   width: calc(33.333% - 0.666rem);
   object-fit: cover;
+}
+
+label {
+  margin-top: calc-rem(50);
+  text-align: center;
+  color: var(--color-grijs-10);
 }
 </style>
