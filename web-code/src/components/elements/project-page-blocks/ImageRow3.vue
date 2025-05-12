a54e54aa1c6ed6e54a51bdad7ebc0bf3e912a5d3
Luca <hello@lucaelsen.de>
2025-05-12 16:39:43 +0200

    fix image-row-3 label placement

--- web-code/src/components/elements/project-page-blocks/ImageRow3.vue
+++ web-code/src/components/elements/project-page-blocks/ImageRow3.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="project-block image-row-3 col-span-12">
-    <div
-      v-for="(item, index) in blockData.content.images"
-      :key="index"
-      class="image-label-combo col-span-4"
-    >
-      <img
-        :src="item.image.url"
-        :alt="item.image.alt"
-        class="image-row-3__image"
-      />
-      <label
-        :id="labelId"
-        v-if="blockData.content.label"
-        v-html="blockData.content.label"
+    <div class="grid-wrapper col-span-12">
+      <div
+        v-for="(item, index) in blockData.content.images"
+        :key="index"
+        class="image-label-combo col-span-4"
       >
-      </label>
+        <img
+          :src="item.image.url"
+          :alt="item.image.alt"
+          class="image-row-3__image"
+        />
+        <label :id="labelId" v-if="item.image.label" v-html="item.image.label">
+        </label>
+      </div>
     </div>
+    <label
+      class="col-span-12"
+      :id="labelId"
+      v-if="blockData.content.label"
+      v-html="blockData.content.label"
+    >
+    </label>
   </div>
 </template>
 
@@ -37,7 +42,6 @@ const labelId = computed<string>(() => {
 
 <style lang="scss" scoped>
 .image-row-3 {
-  display: flex;
   // gap: 1rem;
   // justify-content: center;
   // flex-wrap: wrap;
@@ -45,19 +49,23 @@ const labelId = computed<string>(() => {
   display: grid;
   // grid-template-columns: repeat(3, 1fr);
   grid-template-columns: subgrid;
+  .grid-wrapper {
+    display: grid;
+    // grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: subgrid;
+  }
 }
-
 .image-label-combo {
   .image-row-3__image {
     width: 100%;
     height: auto;
     object-fit: cover;
   }
+}
 
-  label {
-    margin-top: calc-rem(50);
-    text-align: center;
-    color: var(--color-grijs-10);
-  }
+label {
+  margin-top: calc-rem(50);
+  text-align: center;
+  color: var(--color-grijs-10);
 }
 </style>
