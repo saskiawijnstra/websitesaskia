9c3ac9192fa990bb462404020dce57139c018e54
Luca <hello@lucaelsen.de>
2025-05-28 11:16:20 +0200

    add jump links

---
+++ web-code/src/components/elements/LinkArrow.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="link-arrow" :class="`color-scheme-${colorScheme}`">
+    <arrow />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import arrow from "@/assets/images/arrow-right.svg";
+import { useUiStore } from "../../stores/uiStore";
+import { storeToRefs } from "pinia";
+
+const uiStore = useUiStore();
+
+const { colorScheme } = storeToRefs(uiStore);
+</script>
+
+<style lang="scss" scoped>
+.link-arrow {
+  height: 32px;
+  width: 32px;
+  display: grid;
+  flex-shrink: 0;
+  place-items: center;
+  border-radius: 50%;
+  background-color: #1a1a1a;
+
+  &.color-scheme-light {
+    background-color: var(--color-grijs-6);
+
+    &::v-deep(path) {
+      fill: var(--color-niet-zwart);
+    }
+  }
+}
+</style>
--- web-code/src/components/pages/AboutView.vue
+++ web-code/src/components/pages/AboutView.vue
@@ -25,7 +25,41 @@
       <p v-html="data.introduction.text.article[locale]"></p>
     </div>
 
-    <div class="jump-links col-span-3"></div>
+    <div class="jump-links col-span-3">
+      <div
+        class="jump-link"
+        v-for="(linkData, index) in data.introduction['jump-links']"
+        :key="index"
+      >
+        <link-arrow />
+        <dl>
+          <dt>
+            {{ linkData.annotation[locale] }}
+          </dt>
+          <dd>
+            <template v-if="linkData.type === 'annotated-link'">
+              <router-link
+                v-html="parseContent(linkData.name[locale])"
+                :to="`/work/${linkData.target}`"
+              >
+              </router-link>
+            </template>
+            <template v-if="linkData.type === 'link-list'">
+              <span v-for="(link, index) in linkData.links" :key="link.target">
+                <router-link
+                  v-html="parseContent(link.name[locale])"
+                  :to="`/work/${link.target}`"
+                >
+                </router-link>
+                <template v-if="index < linkData.links.length - 1">
+                  {{ ", " }}
+                </template>
+              </span>
+            </template>
+          </dd>
+        </dl>
+      </div>
+    </div>
   </article>
 </template>
 
@@ -35,10 +69,17 @@ import QuoteBlock from "../elements/project-page-blocks/QuoteBlock.vue";
 import { computed } from "vue";
 import { useI18n } from "vue-i18n";
 import FullImage from "../elements/project-page-blocks/FullImage.vue";
-
+import { useMarkdownParser } from "../elements/useMarkdownParser";
+import LinkArrow from "../elements/LinkArrow.vue";
 const data = computed(() => yamlDataAbout);
 
 const { locale } = useI18n();
+
+const { formatText } = useMarkdownParser();
+
+function parseContent(content: string): string {
+  return formatText(content);
+}
 </script>
 
 <style lang="scss" scoped>
@@ -59,5 +100,29 @@ const { locale } = useI18n();
       text-decoration: underline;
     }
   }
+}
+
+.jump-links {
+  align-self: center;
+  display: flex;
+  gap: 109px;
+  flex-direction: column;
+
+  .jump-link {
+    display: flex;
+    align-items: center;
+    gap: calc-rem(16);
+  }
+
+  dt {
+    margin-bottom: 5px;
+  }
+}
+
+dd,
+dt,
+dl {
+  margin: 0;
+  padding: 0;
 }
 </style>
