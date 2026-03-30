cecf9d5513b674e4821004fd8e166f41c8bdf2e3
saskiawijnstra <40322285+saskiawijnstra@users.noreply.github.com>
2026-03-30 14:17:35 +0200

    proberen

--- src/components/pages/WorkView.vue
+++ src/components/pages/WorkView.vue
@@ -4,6 +4,29 @@
     <article class="">
       <!-- Discover my Work -->
 
+      <div class="more-designs grid-container">
+        <h2 class="h1 col-span-6">
+          {{ yamlDataWork["more-designs"].title[locale] }}
+        </h2>
+        <ul class="design-list col-span-12-start-1">
+          <li class="col-span-4" v-for="entry in moreDesignsData">
+            <component
+              :is="entry['no-link'] === true ? 'div' : 'router-link'"
+              :to="`/work/${entry.id}`"
+            >
+              <img
+                :src="entry.thumbnail.path"
+                :alt="entry.thumbnail.alt[locale]"
+              />
+              <p>
+                <span v-html="entry.title[locale]"></span>
+                <ArrowRight v-if="!entry['no-link']" class="arrow-icon" />
+              </p>
+            </component>
+          </li>
+        </ul>
+      </div>
+
       <div class="discover-work-list grid-container">
         <div class="introduction col-span-6" id="work">
           <h1 v-html="yamlDataWork.discover.title[locale]"></h1>
