501f152cf19a89ebbc24538e38144702a42bf918
saskiawijnstra <40322285+saskiawijnstra@users.noreply.github.com>
2026-06-05 18:46:46 +0200

    PijlW

--- src/components/pages/WorkView.vue
+++ src/components/pages/WorkView.vue
@@ -22,7 +22,7 @@
               />
         <p>
           <span v-html="entry.title[locale]"></span>
-          <ArrowRight v-if="!entry['no-link']" class="arrow-icon" />
+          <PijlW v-if="!entry['no-link']" class="arrow-icon" />
         </p>
         </component>
         </li>
@@ -109,7 +109,7 @@
                   <p class="caption">
                     {{ project.title[locale] }}
 
-                    <ArrowRight class="arrow-icon" />
+                    <PijlW class="arrow-icon" />
                   </p>
                 </router-link>
               </div>
@@ -136,7 +136,7 @@
               />
               <p>
                 <span v-html="entry.title[locale]"></span>
-                <ArrowRight v-if="!entry['no-link']" class="arrow-icon" />
+                <PijlW v-if="!entry['no-link']" class="arrow-icon" />
               </p>
             </component>
           </li>
@@ -156,6 +156,7 @@ import { computed, nextTick, onMounted, onUnmounted, ref } from "vue";
 import { useUiStore } from "../../stores/uiStore";
 import { storeToRefs } from "pinia";
 import ArrowRight from "@/assets/images/arrow-right.svg?component";
+import PijlW from "@/assets/images/pijl-w.svg?component";
 import LandingPage from "../elements/LandingPage.vue";
 import projectThumbnail from "../elements/work/project-thumbnail.vue";
 
