5924dd76618b965eac154a90f4209e555e6029fd
saskiawijnstra <40322285+saskiawijnstra@users.noreply.github.com>
2026-09-03 14:07:19 +0200

    kleur

--- src/components/header/locale-switch.vue
+++ src/components/header/locale-switch.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     class="locale-switch"
+    :class="`color-scheme-${colorScheme}`"
     role="radiogroup"
     :aria-label="t('settings.select-language-accessibility-label')"
   >
@@ -12,7 +13,7 @@
       role="radio"
     >
       <input type="radio" :value="locale" v-model="selected" class="sr-only" />
-      {{ locale }}
+      <span class="locale-text">{{ locale }}</span>
     </label>
   </div>
 </template>
@@ -20,10 +21,15 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import { useI18n } from "vue-i18n";
+import { useUiStore } from "../../stores/uiStore";
+import { storeToRefs } from "pinia";
 
 const { t } = useI18n();
 
 const { availableLocales, locale: currentLocale } = useI18n();
+
+const uiStore = useUiStore();
+const { colorScheme } = storeToRefs(uiStore);
 
 const selected = computed({
   get: () => currentLocale.value,
@@ -41,25 +47,50 @@ const selected = computed({
 
 .locale-option {
   cursor: pointer;
-  color: var(--color-creme);
   transition: color 0.2s;
   text-transform: uppercase;
   position: relative;
-  padding-bottom: 4px;
-  border-bottom: 2px solid transparent;
 
   &.active {
-    color: var(--color-default-text);
-    opacity: 60%;
-    border-bottom-color: currentColor;
+    cursor: default;
   }
 
   &:not(:last-child)::after {
     content: " / ";
-    color: var(--color-creme);
+    color: currentColor;
     margin-left: 0.25rem;
     margin-right: 0.25rem;
   }
+}
+
+.locale-switch.color-scheme-light {
+  .locale-option {
+    color: var(--color-bruin);
+
+    &.active {
+      color: var(--color-roest);
+    }
+  }
+}
+
+.locale-switch.color-scheme-dark {
+  .locale-option {
+    color: var(--color-creme);
+
+    &.active {
+      color: var(--color-default-text);
+      opacity: 60%;
+    }
+  }
+}
+
+.locale-text {
+  padding-bottom: 4px;
+  border-bottom: 2px solid transparent;
+}
+
+.locale-option.active .locale-text {
+  border-bottom-color: currentColor;
 }
 
 .locale-option:has(input.sr-only:focus-visible) {
